81eed562b7b25eab02c39e9af0060d193bc3e0ef
Pablo Mombo Rasero <One@MacBook-Pro-de-Pablo.local>
2019-02-27 10:20:03 +0100

    service worker

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,22 @@
+// register service worker - BEGINNING
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/bootstrap-resto-website/assets/JS/sw.js', { scope: '/bootstrap-resto-website/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+// register service worker – END
+
 /* heure du jour */
 var today = new Date();
 	var d = today.getDate();
